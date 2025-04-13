5c85706e77da68e5d75e60f892684038c425580e
Shana Lynch <shana.lynch21@gmail.com>
2025-04-12 22:19:47 -0400

    Initial commit

--- index.js
+++ index.js
@@ -14,9 +14,47 @@
 //! After creating the four variables mentioned above, uncomment the following lines
 //* This way you can export them to the test file, this is essential for the tests to work
 
+const temperatures = [
+  { celsius: 21 },
+  { fahrenheit: 68 },
+  { celsius: 23 },
+  { fahrenheit: 75.2 },
+  { celsius: 19 },
+  { fahrenheit: 77 },
+  { celsius: 16 },
+  { fahrenheit: 69.8 },
+  { fahrenheit: 73.4 },
+  { celsius: 26 }
+];
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+let celsiusTemps = temperatures.map(temp =>
+  temp.celsius !== undefined ? temp.celsius : toCelsius(temp.fahrenheit)
+);
+
+let tot_temperature_in_celsius = celsiusTemps.reduce((a, b) => a + b, 0);
+let avg_temperature_in_celsius = tot_temperature_in_celsius / celsiusTemps.length;
+
+let tot_temperature_in_fahrenheit = celsiusTemps
+  .map(toFahrenheit)
+  .reduce((a, b) => a + b, 0);
+let avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / celsiusTemps.length;
+
+console.log("total temperature in Celsius:", tot_temperature_in_celsius);
+console.log("total temperature in Fahrenheit:", tot_temperature_in_fahrenheit);
+console.log("average temperature in Celsius:", avg_temperature_in_celsius);
+console.log("average temperature in Fahrenheit:", avg_temperature_in_fahrenheit);
+
 module.exports = {
-    // tot_temperature_in_fahrenheit,
-    // tot_temperature_in_celsius,
-    // avg_temperature_in_fahrenheit,
-    // avg_temperature_in_celsius
+  tot_temperature_in_fahrenheit,
+  tot_temperature_in_celsius,
+  avg_temperature_in_fahrenheit,
+  avg_temperature_in_celsius
 };
